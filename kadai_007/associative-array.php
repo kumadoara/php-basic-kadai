d6638ea7b2f3d58a7e21302adf885f5f876ad3be
kumadoara <kw2st68@hotmail.com>
2023-10-24 10:58:29 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+ 
+<body>
+    <p>
+        <?php
+        // 連想配列を作成し、変数へ代入する
+        $product_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+        // 値を出力する
+        print_r($product_data);
+
+        ?>
+    </p>
+</body>
+
+</html>
+
+
+
