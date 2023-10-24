315c434cc34dc7bea0d790a22bf9ead63fcdfd55
kumadoara <kw2st68@hotmail.com>
2023-10-24 13:50:57 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+ 
+<body>
+    <p>
+        <?php
+        $prodct_names =[
+            '名前' => '玉ねぎ',
+            '値段' => 200,
+            '産地' => '北海道'
+        ];
+
+        foreach ($prodct_names as $key => $value) {
+            echo "{$key} : {$value} <br>";
+        }
+        ?>
+    </P>
+</body>
+</html>
